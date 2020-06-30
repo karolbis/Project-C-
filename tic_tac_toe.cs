ad99aab7b932888ae50dd3f43a070d1c362232e0
karolbis <61646274+karolbis@users.noreply.github.com>
2020-06-30 14:28:26 +0200

    Update tic_tac_toe.cs

--- tic_tac_toe.cs
+++ tic_tac_toe.cs
@@ -2,39 +2,155 @@ using System;
 using System.Linq;
 using System.Text;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Security.Cryptography.X509Certificates;
 
-namespace TiCTacToe1
+namespace TicTacToe1
+
 {
+
     class Program
+
     {
+        static char[] NumTablica = 
+        { 
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' 
+        };
+
+        static int player = 1;  
+
+        static int choice;    
+
+        static int flag = 0;
         static void Main(string[] args)
+
         {
-            Start();
-            while (Wygrany() == false)
+
+            do
+
             {
-                Gracze("x");
-                if (Wygrany() == true)
-                    break;
-                Gracze("0");
+                Start();
+
+                Console.WriteLine("Player 1 is X || Player 2 is O");
+
+                Console.WriteLine("\n");
+
+                
+
+                if (player % 2 == 0) 
+
+                {
+
+                    Console.WriteLine("Player 2 Chance");
+
+                }
+
+                else
+
+                {
+
+                    Console.WriteLine("Player 1 Chance");
+
+                }
+
+                Console.WriteLine("\n");
+
+                Tablica();  
+
+                choice = int.Parse(Console.ReadLine());   
+
+                Console.Clear();
+
+                 
+
+                if (NumTablica[choice] != 'X' && NumTablica[choice] != 'O')
+
+                {
+
+                    if (player % 2 == 0)   
+
+                    {
+
+                        NumTablica[choice] = 'O';
+
+                        player++;
+
+                    }
+
+                    else
+
+                    {
+
+                        NumTablica[choice] = 'X';
+
+                        player++;
+
+                    }
+
+                }
+
+                else   
+
+                {
+
+                    Console.WriteLine("Already taken place", choice, NumTablica[choice]);
+
+                    Console.WriteLine("\n");
+
+                }
+
+                flag = Wygrana();  
+
+            } while (flag != 1 && flag != -1); 
+
+
+
+            Console.Clear();
+
+            Tablica(); 
+
+
+
+            if (flag == 1)
+
+            {
+
+                Console.WriteLine("Player {0} has won", (player % 2) + 1);
+
             }
+
+            else  
+
+            {
+
+                Console.WriteLine("Draw");
+
+            }
+
+            Console.ReadLine();
+
         }
 
 
+        private static void Tablica()
 
-
-
-
-
-        static String[] NumTablica = new String[9];
-        static void zmienna()
         {
-            for (int i = 0; i < 9; i++)
-            {
-                NumTablica[i] = i.ToString();
-            }
+
+            
+            Console.WriteLine("  .........................");
+            Console.WriteLine("  :       :       :       :");
+            Console.WriteLine("  :   {0}   :   {1}   :   {2}   :", NumTablica[1], NumTablica[2], NumTablica[3]);
+            Console.WriteLine("  :       :       :       :");
+            Console.WriteLine("  .........................");
+            Console.WriteLine("  :       :       :       :");
+            Console.WriteLine("  :   {0}   :   {1}   :   {2}   :", NumTablica[4], NumTablica[5], NumTablica[6]);
+            Console.WriteLine("  :       :       :       :");
+            Console.WriteLine("  .........................");
+            Console.WriteLine("  :       :       :       :");
+            Console.WriteLine("  :   {0}   :   {1}   :   {2}   :", NumTablica[7], NumTablica[8], NumTablica[9]);
+            Console.WriteLine("  :       :       :       :");
+            Console.WriteLine("  .........................");
+            
         }
+
         public static void Start()
         {
             Console.ForegroundColor = ConsoleColor.Blue;
@@ -60,29 +176,13 @@ namespace TiCTacToe1
                 "\nMay the best win!!!" +
                 "\nPress any key to continue");
             Console.ReadKey();
-            Console.Clear();
+          
+        }
+             
 
-            static void Tablica()
+            private static int Wygrana()
             {
-                Console.Clear();
-                Console.WriteLine("  .........................");
-                Console.WriteLine("  :       :       :       :");
-                Console.WriteLine("  :   1   :   2   :   3   :", NumTablica[0], NumTablica[1], NumTablica[2]);
-                Console.WriteLine("  :       :       :       :");
-                Console.WriteLine("  .........................");
-                Console.WriteLine("  :       :       :       :");
-                Console.WriteLine("  :   1   :   2   :   3   :", NumTablica[3], NumTablica[4], NumTablica[5]);
-                Console.WriteLine("  :       :       :       :");
-                Console.WriteLine("  .........................");
-                Console.WriteLine("  :       :       :       :");
-                Console.WriteLine("  :   1   :   2   :   3   :", NumTablica[6], NumTablica[7], NumTablica[8]);
-                Console.WriteLine("  :       :       :       :");
-                Console.WriteLine("  .........................");
-
-
-            }
-            
-            
+            // mechanizm wygranej if else
 
             }
     }
