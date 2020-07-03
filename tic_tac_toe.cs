fd0740cf82da814d0efc1e428f4969262d9bd738
karolbis <61646274+karolbis@users.noreply.github.com>
2020-07-03 19:38:47 +0200

    Update tic_tac_toe.cs

--- tic_tac_toe.cs
+++ tic_tac_toe.cs
@@ -18,16 +18,37 @@ namespace TicTacToe1
         static int player = 1;
 
         static int choice;
-
+        static bool isEnd;
         static int flag = 0;
         static void Main(string[] args)
         {
             Start();
-            
+
             do
             {
-                
-            Console.WriteLine("\n");
+                PlayGame();
+
+
+                Console.WriteLine("Would you play to play again? Y or N");
+            } while (Console.ReadLine().ToLower() == "y");
+            if (Console.ReadLine().ToLower() == "n")
+                Console.WriteLine("\nThanks for playing!");
+
+
+        }
+
+        public static void PlayGame()
+        {
+            if (isEnd)
+            {
+                Program.NumTablica = new char[]{
+                    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+        };
+
+            }
+            do
+            {
+                Console.WriteLine("\n");
                 Console.WriteLine("Player 1 is X || Player 2 is O");
 
                 Console.WriteLine("\n");
@@ -56,50 +77,46 @@ namespace TicTacToe1
                         NumTablica[choice] = 'X';
                         player++;
                     }
-                    
-                    
-
-
                 }
 
-            else
-            {
-            Console.WriteLine("ALREADY TAKEN PLACE, PLEASE PICK OTHER", choice, NumTablica[choice]);
-            }
+                else
+                {
+                    Console.WriteLine("ALREADY TAKEN PLACE, PLEASE PICK OTHER", choice, NumTablica[choice]);
+                }
 
-            flag = Wygrana();
+                flag = Wygrana();
 
             } while (flag != 1 && flag != -1);
 
             Console.Clear();
-            
+
             Tablica();
-            
+
             if (flag == 1)
             {
                 Console.WriteLine("Player {0} has won", (player % 2) + 1);
                 Console.WriteLine("Here is your trophy:");
-                Console.WriteLine("  __________     "             );
-                Console.WriteLine(" |    XOX   |    "              );
-                Console.WriteLine("  |   OXO  |     "       );
-                Console.WriteLine("   |  XOX |      "       );
-                Console.WriteLine("    |____|       "           );
-                Console.WriteLine("      ||         "            );
-                Console.WriteLine("      ||         "             );
-                Console.WriteLine("     ||||        "                 );
+                Console.WriteLine("  __________     ");
+                Console.WriteLine(" |    XOX   |    ");
+                Console.WriteLine("  |   OXO  |     ");
+                Console.WriteLine("   |  XOX |      ");
+                Console.WriteLine("    |____|       ");
+                Console.WriteLine("      ||         ");
+                Console.WriteLine("      ||         ");
+                Console.WriteLine("     ||||        ");
             }
             else
-           { 
-           Console.WriteLine("Draw");
-           }
-            Console.ReadLine();
-           }
-           
+            {
+                Console.WriteLine("Draw");
+            }
 
-        
+        }
+
+
+
         private static void Tablica()
 
-           {
+        {
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("  .........................");
@@ -142,53 +159,60 @@ namespace TicTacToe1
                 "\nThe frist player who score three same signatures in a row is the winner of a game." +
                 "\nMay the best win!!!" +
                 "\nPress any key to continue");
-        Console.ReadKey();
+            Console.ReadKey();
         }
 
-        
 
         private static int Wygrana()
         {
             //Wszystkie wygrane w pionie
             if (NumTablica[1] == NumTablica[4] && NumTablica[4] == NumTablica[7])
             {
+                isEnd = true;
                 return 1;
             }
             else if
             (NumTablica[2] == NumTablica[5] && NumTablica[5] == NumTablica[8])
             {
+                isEnd = true;
                 return 1;
             }
             else if
             (NumTablica[3] == NumTablica[6] && NumTablica[6] == NumTablica[9])
             {
+                isEnd = true;
                 return 1;
             }
             //Wszystkie wygrane w poziomie
             else if
             (NumTablica[1] == NumTablica[2] && NumTablica[2] == NumTablica[3])
             {
+                isEnd = true;
                 return 1;
             }
             else if
             (NumTablica[4] == NumTablica[5] && NumTablica[5] == NumTablica[6])
             {
+                isEnd = true;
                 return 1;
             }
             else if
             (NumTablica[7] == NumTablica[8] && NumTablica[8] == NumTablica[9])
             {
+                isEnd = true;
                 return 1;
             }
             //Wszystkie wygrane po skosie
             else if
             (NumTablica[3] == NumTablica[5] && NumTablica[5] == NumTablica[7])
             {
+                isEnd = true;
                 return 1;
             }
             else if
             (NumTablica[1] == NumTablica[5] && NumTablica[5] == NumTablica[9])
             {
+                isEnd = true;
                 return 1;
             }
             //Remisy 
@@ -196,18 +220,16 @@ namespace TicTacToe1
             (NumTablica[1] != '1' && NumTablica[2] != '2' && NumTablica[3] != '3' && NumTablica[4] != '4' && NumTablica[5] != '5' && NumTablica[6] != '6' && NumTablica[7] != '7' && NumTablica[8] != '8' && NumTablica[9] != '9')
 
             {
-
+                isEnd = true;
                 return -1;
 
             }
             else
             {
+                isEnd = true;
                 return 0;
             }
-            
+
         }
     }
 }
-
-
-
